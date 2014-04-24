95fdf93af9a33f9c355c6dda4f63053f21b962ed
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-04-23 22:32:22 -0700

    add association expectations to User and Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,5 @@ require_relative '../spec_helper'
 
 describe Comment do
   it { should belong_to :post }
+  it { should belong_to :user }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require_relative '../spec_helper'
 
 describe User do
   it { should have_many :posts }
+  it { should have_many :comments }
 end
