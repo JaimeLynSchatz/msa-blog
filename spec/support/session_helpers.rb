66163ec5221b7ab17f81e6e0c161a126dd2abd13
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-04-24 11:25:18 -0700

    attempted workaround for sign_in vs. log_in method name

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -9,4 +9,17 @@ def log_in(user)
   end
 
   user
+end
+
+def sign_in(user)
+  visit new_user_session_path
+
+  within 'form.new_user' do
+    fill_in 'Email', with: user.Email
+    fill_in 'Password', with: 'password'
+
+    click_link_or_button 'Sign in'
   end
+
+  user
+end
