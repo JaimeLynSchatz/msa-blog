90a8c0445e25050532cf00b2b9deff888efc1ab8
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-04-24 21:07:12 -0700

    test for sign in and sign out

---
+++ spec/features/user_can_sign_out_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'spec_helper'
+
+feature 'Users' do
+  background do
+    @user = create :user
+  end
+
+  scenario 'can sign in' do
+
+    visit '/'
+
+    click_link_or_button 'Sign in'
+
+    fill_in 'Email', :with => @user.email
+    fill_in 'user_passoword', :with => @user.user_passoword
+
+    click_link_or_button 'Sign in'
+
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  scenario 'can sign out' do
+    log_in @user
+
+    visit '/'
+
+    click_link_or_button 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully'
+  end
+end
