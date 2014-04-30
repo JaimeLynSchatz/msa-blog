e2303964d075243bf3220fef04a12ff4ebea34b6
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-04-30 09:14:10 -0700

    fixed typo in test

--- spec/features/hello_world_spec.rb
+++ spec/features/hello_world_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../spec_helper'
 
 feature 'The world' do
-  scenario 'sees Hello, Raisl!' do
+  scenario 'sees Hello, Rails!' do
     visit '/'
     expect(page).to have_content('Hello, Rails!')
   end
