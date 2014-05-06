73d6ace377e45e695b93ac41f36065f7ca3bc623
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-05-06 12:17:06 -0700

    try user_email instead of Email

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -2,7 +2,7 @@ def log_in(user)
   visit new_user_session_path
 
   within 'form.new_user' do
-    fill_in 'Email', with: user.Email
+    fill_in 'user_email', with: user.Email
     fill_in 'Password', with: 'password'
 
     click_link_or_button 'Sign in'
